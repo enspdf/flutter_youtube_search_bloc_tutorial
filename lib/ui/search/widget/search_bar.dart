ba3d9ced114dfd84cb8da31bf37e36761055b172
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-04-10 21:21:08 -0500

    Paginated ListView following bloc

---
+++ lib/ui/search/widget/search_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_search_tutorial/ui/search/widget/search_field.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 5.0,
+        ),
+        child: SearchField(),
+      ),
+    );
+  }
+}
